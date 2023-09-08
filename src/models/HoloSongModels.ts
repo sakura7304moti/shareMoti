74e44838ee61b61efcf58d3591b71ff45e55e8bf
sakura7304moti <sakura7304moti@gmail.com>
2023-09-09 01:49:23 +0900

    vue-youtubeを追加して、最低限ページを完成

--- src/models/HoloSongModels.ts
+++ src/models/HoloSongModels.ts
@@ -3,22 +3,55 @@ import api from 'src/api/HoloSongApi';
 import { ref } from 'vue';
 
 export function useHoloSongModel() {
+  const { getVideoId, getImageLink } = subModel();
+
   const quasar = useQuasar();
   const records = ref([] as DataState[]);
+  const pageState = ref({
+    selectLink: '',
+    pageNo: 1,
+    totalPages: 1,
+    pageSize: 20,
+    records: [] as DataState[],
+  } as PageState);
+
+  const isLoading = ref(false);
+  const searchOptionShow = ref(false);
 
   const select = async function () {
+    isLoading.value = true;
     records.value.splice(0);
     await api
       .search()
       .then((response) => {
         if (response) {
-          console.log('select', response);
+          //レコードのリスト
           response.records.forEach((it) => {
             if (it.link.includes('you') && !it.detail.includes('非公開')) {
-              records.value.push(it);
+              const addRec = {
+                date: it.date,
+                member: it.member,
+                link: it.link,
+                songName: it.songName,
+                detail: it.detail,
+                imageLink: getImageLink(it.link),
+                videoId: getVideoId(it.link),
+              } as DataState;
+              records.value.push(addRec);
             }
           });
-          console.log('datastate', records.value);
+          records.value.reverse();
+          console.log('add list', records.value.length);
+
+          //ページング用オブジェクトを準備
+          pageState.value.totalPages = Math.ceil(
+            records.value.length / pageState.value.pageSize
+          );
+          selectPage();
+
+          //リスト
+          const arr = [...new Set(records.value.map((it) => it.member))];
+          console.log(arr);
         }
       })
       .catch((e) => {
@@ -29,11 +62,55 @@ export function useHoloSongModel() {
           message: 'データの取得に失敗しました',
         });
       });
+    isLoading.value = false;
   };
+
+  const selectPage = function () {
+    pageState.value.records.splice(0);
+    //1ページ目 -> 0 ~ 19
+    //2ページ目 -> 20 ~ 39
+    //3ページ目 -> 40 ~ 59
+    const startIndex =
+      Math.max(pageState.value.pageNo - 1, -1) * pageState.value.pageSize;
+    const lastIndex = Math.min(
+      startIndex + pageState.value.pageSize - 1,
+      records.value.length
+    );
+    records.value.slice(startIndex, lastIndex).forEach((rec) => {
+      pageState.value.records.push(rec);
+    });
+    console.log('data', pageState.value);
+  };
+
   return {
     quasar,
     records,
+    pageState,
+    isLoading,
+    searchOptionShow,
     select,
+    selectPage,
+  };
+}
+
+function subModel() {
+  const getVideoId = function (url: string) {
+    const v = url.split('v=')[1];
+    if (v != undefined) {
+      return v;
+    } else {
+      const s = url.split('/')[url.split('/').length - 1];
+      return s;
+    }
+  };
+
+  const getImageLink = function (url: string) {
+    const baseUrl = 'https://img.youtube.com/vi/query/maxresdefault.jpg';
+    return baseUrl.replace('query', getVideoId(url));
+  };
+  return {
+    getVideoId,
+    getImageLink,
   };
 }
 interface DataState {
@@ -42,4 +119,13 @@ interface DataState {
   link: string;
   songName: string;
   detail: string;
+  imageLink: string;
+  videoId: string;
+}
+interface PageState {
+  selectLink: string;
+  pageNo: number;
+  totalPages: number;
+  pageSize: number;
+  records: DataState[];
 }
--- src/pages/HoloSongPage.vue
+++ src/pages/HoloSongPage.vue
@@ -1,23 +1,119 @@
 <template>
   <q-page class="">
     <div class="q-pb-md">
+      <!--1/3 タイトル-->
       <div class="text-h5">ホロライブ歌みたまとめ</div>
+      <div id="holo-page-loading" v-if="isLoading" class="row q-gutter-md">
+        <q-spinner color="primary" size="3em" />
+        <div class="text-subtitle1 text-primary">検索中...</div>
+      </div>
+
+      <div class="q-pa-md row q-gutter-md" v-if="pageState.selectLink != ''">
+        <YouTube
+          :src="pageState.selectLink"
+          @ready="true"
+          ref="youtube"
+          :vars="{ autoplay: 1, rel: 0 }"
+        />
+      </div>
+
+      <!--2/3表示条件-->
+      <div v-if="!isLoading">
+        <div class="row q-gutter-md" v-if="pageState.totalPages != 1">
+          <q-pagination
+            v-model="pageState.pageNo"
+            :max="pageState.totalPages"
+            direction-links
+            :max-pages="10"
+            @click.prevent="selectPage"
+            class="q-pb-md"
+            outlined
+          />
+          <a href="#" @click.prevent.stop="searchOptionShow = true"
+            ><q-icon name="view_list" color="primary" size="md"
+          /></a>
+        </div>
+
+        <!--詳細なモーダル-->
+        <q-dialog v-model="searchOptionShow">
+          <q-card style="width: 400px">
+            <q-card-section>
+              <div class="text-h5">ページ選択</div>
+              <hr />
+              <q-pagination
+                v-model="pageState.pageNo"
+                :max="pageState.totalPages"
+                @click.prevent="selectPage"
+                class="q-pb-md"
+                outlined
+                style="width: 300px"
+              />
+            </q-card-section>
+          </q-card>
+        </q-dialog>
+      </div>
+
+      <!--3/3　サムネイルリスト-->
+      <div id="holo-song-cards" style="min-height: 780px">
+        <div class="row q-gutter-md">
+          <div v-for="rec in pageState.records" :key="rec.link" class="q-pa-sm">
+            <q-card style="width: 100%; max-width: 250px">
+              <q-card-section>
+                <a
+                  :href="rec.link"
+                  @click.prevent="pageState.selectLink = rec.link"
+                  ><img
+                    :src="rec.imageLink"
+                    style="width: 100%; max-width: 250px"
+                /></a>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+      <div class="row q-gutter-md q-pt-md" v-if="pageState.totalPages != 1">
+        <q-pagination
+          v-model="pageState.pageNo"
+          :max="pageState.totalPages"
+          direction-links
+          :max-pages="10"
+          @click.prevent="selectPage"
+          class="q-pb-md"
+          outlined
+        />
+        <a href="#" @click.prevent.stop="searchOptionShow = true"
+          ><q-icon name="view_list" color="primary" size="md"
+        /></a>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { useHoloSongModel } from 'src/models/HoloSongModels';
-import { computed, defineComponent, ref } from 'vue';
-
+import { defineComponent } from 'vue';
+import YouTube from 'vue3-youtube';
 export default defineComponent({
   name: 'holosong-page',
+  components: { YouTube },
   setup() {
-    const { quasar, records, select } = useHoloSongModel();
+    const {
+      quasar,
+      records,
+      pageState,
+      isLoading,
+      searchOptionShow,
+      select,
+      selectPage,
+    } = useHoloSongModel();
     select();
 
     return {
       records,
+      pageState,
+      isLoading,
+      searchOptionShow,
+      selectPage,
     };
   },
 });
