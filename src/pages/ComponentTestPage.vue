90b6e9b62d4986c1658d9a96e644439df997450b
sakura7304moti <sakura7304moti@gmail.com>
2023-10-25 22:36:44 +0900

    焼き直し条約のテーブルのComponent

--- src/pages/ComponentTestPage.vue
+++ src/pages/ComponentTestPage.vue
@@ -6,11 +6,11 @@
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import NameListTable from 'components/tables/NameListTable.vue';
+import YakiListTable from 'components/tables/YakiListTable.vue';
 export default defineComponent({
   name: 'component-test',
   components: {
-    'custom-table': NameListTable,
+    'custom-table': YakiListTable,
   },
   setup() {
     const pageHeight = ref(document.documentElement.scrollHeight * 0.75);
