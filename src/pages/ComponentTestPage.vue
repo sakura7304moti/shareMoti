9b65925eb62068dea9de5bec42396d1d4f11f8b6
sakura7304moti <sakura7304moti@gmail.com>
2023-10-25 22:25:46 +0900

    あだ名一覧のテーブルのcomponent

--- src/pages/ComponentTestPage.vue
+++ src/pages/ComponentTestPage.vue
@@ -1,16 +1,16 @@
 <template>
   <q-page class="">
     <div class="q-pb-md text-h5">Component Test Page</div>
-    <word-list-table v-model="filter" :height="pageHeight" />
+    <custom-table v-model="filter" :height="pageHeight" />
   </q-page>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import WordListTable from 'components/tables/WordListTable.vue';
+import NameListTable from 'components/tables/NameListTable.vue';
 export default defineComponent({
   name: 'component-test',
   components: {
-    'word-list-table': WordListTable,
+    'custom-table': NameListTable,
   },
   setup() {
     const pageHeight = ref(document.documentElement.scrollHeight * 0.75);
